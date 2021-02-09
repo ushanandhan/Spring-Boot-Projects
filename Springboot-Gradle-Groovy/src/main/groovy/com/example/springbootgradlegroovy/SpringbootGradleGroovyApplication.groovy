6bab1053c58fe4ab779a77e4f25f0b756b3e58e2
Ushanandhan Murugesu <ushanandhan.murugesu@rntbci.com>
2021-02-09 14:29:03 +0530

    SpringBoot-gradle-groovy related code committed

---
+++ Springboot-Gradle-Groovy/src/main/groovy/com/example/springbootgradlegroovy/SpringbootGradleGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.springbootgradlegroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringbootGradleGroovyApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringbootGradleGroovyApplication, args)
+    }
+
+}
---
+++ Springboot-Gradle-Groovy/src/test/groovy/com/example/springbootgradlegroovy/SpringbootGradleGroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.springbootgradlegroovy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootGradleGroovyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
