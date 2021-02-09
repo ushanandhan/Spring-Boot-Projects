739d22a16a4cf5096f29fef63a5b9bd411a744ea
ushanandhan <ariyaushan@gmail.com>
2021-02-09 18:51:00 +0530

    Springboot Gradle Groovy Related code commited

--- Springboot-Gradle-Groovy/src/main/groovy/com/example/SpringbootGradleGroovyApplication.groovy
+++ Springboot-Gradle-Groovy/src/main/groovy/com/example/SpringbootGradleGroovyApplication.groovy
@@ -1,4 +1,4 @@
-package com.example.springbootgradlegroovy
+package com.example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
