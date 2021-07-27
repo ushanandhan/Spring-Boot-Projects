f881fbf65be33c250e6a6467d49a64cf0b159c82
Ushanandhan Murugesu <ushanandhan.murugesu@rntbci.com>
2021-07-27 10:39:00 +0530

    Cron job Scheduler related changes committed

--- SpringBoot-Email-Thymeleaf/src/main/java/com/example/config/SchedulerConfig.java
+++ SpringBoot-Email-Thymeleaf/src/main/java/com/example/config/SchedulerConfig.java
@@ -13,7 +13,18 @@ import java.time.LocalDate;
 public class SchedulerConfig {
 
     @Scheduled(cron = "0 5 12 * * MON-FRI") // At 12:05 on every day-of-week from Monday through Friday
-    public void someJob() {
+    public void firstJob() {
+        for (int i = 0; i <10;i++){
+            log.info("[first job] {} at {}  ",i, LocalDate.now());
+        }
+        log.info("Now is "+ LocalDate.now());
+    }
+
+    @Scheduled(cron = "0 5 12 * * MON-FRI") // At 12:05 on every day-of-week from Monday through Friday
+    public void secondJob() {
+        for (int i = 0; i <5;i++){
+            log.info("[second job] {} at {}  ",i, LocalDate.now());
+        }
         log.info("Now is "+ LocalDate.now());
     }
 }
